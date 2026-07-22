0017fe5d9c331b5576735a6e1ad06871521ffbb5
Nông Quốc Khánh <hkg11khanh@gmail.com>
2026-07-22 13:33:44 +0700

    nextjs rewrites as proxy

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -2,6 +2,14 @@
 const nextConfig = {
   /* config options here */
   reactCompiler: true,
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: 'http://103.82.25.74:26090/api/:path*',
+      },
+    ]
+  },
 };
 
 export default nextConfig;
